00589c30a87b5a549ab4a4c6b5205bab0ef7421d
Đỗ Cao Hải Đăng <Docaohaidang83@gmail.com>
2025-10-05 15:51:16 +0700

    xong phần JS ListMovie, DetailMovie, routes

--- BookMovie/src/App.jsx
+++ BookMovie/src/App.jsx
@@ -1,30 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeTemplate from "./MyProject/HomeTemplate";
-import AdminTemplate from "./MyProject/AdminTemplate";
-import AdminPage from "./MyProject/AdminTemplate/adminPage";
-import ListMovie from "./MyProject/HomeTemplate/ListMovie";
-import Homepage from "./MyProject/HomeTemplate/HomePage";
-import Authentication from "./MyProject/AdminTemplate/authenticaiton";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { renderRoutes } from "./routes";
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* HomeTemplate  */}
-          <Route path="" element={<HomeTemplate />}>
-            <Route path="" element={<Homepage />} />
-            <Route path="list-movie" element={<ListMovie />} />
-          </Route>
-
-          {/* AdminTemplate  */}
-          <Route path="admin" element={<AdminTemplate />}>
-            <Route path="adminpage" elemen={<AdminPage />} />
-          </Route>
-          {/* Authen  */}
-          <Route path="auth" element={<Authentication />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>{renderRoutes()}</Routes>
+    </BrowserRouter>
   );
 }
 
--- BookMovie/src/store/index.js
+++ BookMovie/src/store/index.js
@@ -1,4 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import listMovieReducer from "./../MyProject/HomeTemplate/ListMovie/slice";
+import detailReducer from "./../MyProject/HomeTemplate/DetailMovie/slice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    listMovieReducer,
+    detailReducer,
+  },
 });
